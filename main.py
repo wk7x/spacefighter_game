2322ed758577e05db6ae6e16fe86f5f4474f6110
wk7x <aleksei.ostlund@gmail.com>
2025-01-14 23:14:32 +0000

    Added circleshape, player classes. Added PLAYER_RADIUS constant. Added player spawn to main.

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 import sys
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    game_loop(screen)
+    game_loop(screen, clock)
 
-def game_loop(screen):
+def game_loop(screen, clock):
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,7 +25,7 @@ def game_loop(screen):
         
         screen.fill((0, 0, 0))
         pygame.display.flip()
-        
+        dt = clock.tick(60) / 1000
 
 
 
