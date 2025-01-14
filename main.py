ae45783385427c910619cf67dd6958c7c07e1efd
wk7x <aleksei.ostlund@gmail.com>
2025-01-14 22:45:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    game_loop(screen)
+
+def game_loop(screen):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+        
+
+
+
+if __name__ == "__main__":
+    main()
